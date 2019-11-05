7f9720e703682cee28a1e7f7838516710dc8f41b
duy1314 <13632585646@163.com>
2019-11-05 17:01:55 +0800

    添加google的guava缓存的尝试；

--- src/main/java/com/duy/demo/cache/FristCache.java
+++ src/main/java/com/duy/demo/cache/FristCache.java
@@ -15,5 +15,5 @@ public class FristCache {
      * expireAfterAccess设置过期时间，缓存的对象在最后一次被访问后指定时间过期
      * expireAfterWrite设置过期时间，缓存的对象在被放入缓存中指定时间过期
      */
-    public static Cache<String,String> firstCache = CacheBuilder.newBuilder().expireAfterAccess(1, TimeUnit.MINUTES).build();
+    public static Cache<String,String> firstCache = CacheBuilder.newBuilder().maximumSize(10).expireAfterAccess(1, TimeUnit.MINUTES).build();
 }
